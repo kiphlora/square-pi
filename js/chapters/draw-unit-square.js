d8d6391c8753ea125ef6e7eafc16795eac6c366d
kiphlora <bmoran.bam@gmail.com>
2017-09-25 17:17:09 -0400

    update project
    fix css
    remove ScrollTracker and all js chapter files except for the
    init-full-algorithm.js file

--- js/chapters/draw-unit-square.js
+++
@@ -1,7 +0,0 @@
-function drawUnitSquare() {
-	console.log("drawing unit square");
-	svg.selectAll("*").style("opacity", 0);
-
-	svg.select(".unit-square").style("opacity", 1);
-
-}
--- js/chapters/init-full-algorithm.js
+++ js/chapters/init-full-algorithm.js
@@ -1,4 +1,4 @@
-var n = 1000;
+var n = 5000;
 var x = buildCustomArray(n, function(){ return randReal(0,1); });
 var y = buildCustomArray(n, function(){ return randReal(0,1); });
 var points = zip(x,y);
@@ -13,6 +13,11 @@ var points = vecOp(points, function(d,i){
 
 var inCircleCount = sum(quickMap(points, "inCircle"));
 var inCircleProp = inCircleCount / n;
+
+console.log(inCircleProp);
+console.log(4 * inCircleProp);
+console.log(1 - inCircleProp);
+console.log( inCircleProp / (1 - inCircleProp) );
 
 var container = d3.select("#scroll-vis");
 var containerBox = container.node().getBoundingClientRect();
@@ -38,44 +43,72 @@ var yscale = d3.scaleLinear()
 	.range([0, width]);
 
 svg.append("rect")
-	.attr("class", "unit-square")
-	.attr("x", xscale(0))
-	.attr("width", xscale(1) - xscale(0))
-	.attr("y", yscale(0))
-	.attr("height", yscale(1) - yscale(0))
-	.attr("fill", "transparent")
-	.attr("stroke", "black");
+		.attr("class", "unit-square")
+		.attr("x", xscale(0))
+		.attr("width", xscale(1) - xscale(0))
+		.attr("y", yscale(0))
+		.attr("height", yscale(1) - yscale(0))
+		.attr("fill", "transparent")
+		.attr("stroke", "transparent")
+	.transition()
+		.delay(500)
+		.duration(1000)
+		.attr("stroke", "black")
+	.on("end", inscribeCircle);
 
-svg.selectAll(".points").data(points)
-	.enter().append("circle")
-		.attr("class", "points")
-		.attr("cx", function(d,i){ return xscale(d.point[0]); })
-		.attr("cy", function(d,i){ return xscale(d.point[1]); })
-		.attr("r", 2)
-		.attr("fill", function(d,i){ return d.inCircle == 1 ? "#0099ff" : "black"; });
+function buildPoints(){
+	svg.selectAll(".points").data(points)
+		.enter().append("circle")
+			.attr("class", "points")
+			.attr("cx", function(d,i){ return xscale(d.point[0]); })
+			.attr("cy", function(d,i){ return xscale(d.point[1]); })
+			.attr("r", 2)
+			.attr("fill", "transparent")
+		.transition()
+			.delay(function(d,i){ return i * 10000 / points.length; })
+			.duration(100)
+			.attr("fill", function(d,i){ return d.inCircle == 1 ? "#0099ff" : "black"; });	
+			// .attr("fill", "black")
+		// .on("end", inscribeCircle);
+}
 
-svg.append("circle")
-	.attr("class", "inscribed-circle")
-	.attr("cx", xscale(circle.point[0]))
-	.attr("cy", yscale(circle.point[1]))
-	.attr("r", xscale(circle.r))
-	.attr("fill", "#0099ff")
-	.attr("fill-opacity", 0.1)
-	.attr("stroke", "#0099ff");
+function inscribeCircle(){
+	svg.append("circle")
+			.attr("class", "inscribed-circle")
+			.attr("cx", xscale(circle.point[0]))
+			.attr("cy", yscale(circle.point[1]))
+			.attr("r", xscale(circle.r))
+			.attr("fill", "transparent")
+			.attr("stroke", "transparent")
+			.attr("fill-opacity", 0)
+		.transition()
+			.duration(1500)
+			.attr("fill", "#0099ff")
+			.attr("fill-opacity", 0.1)
+			.attr("stroke", "#0099ff")
+		.on("end", buildPoints);
+
+	// svg.selectAll(".points")
+	// 	.transition()
+	// 		.attr("fill", function(d,i){ return d.inCircle === 1 ? "#0099ff" : "black"; });
+}
 
 
-svg.append("line")
-	.attr("class", "radius-line")
-	.attr("x1", xscale(circle.point[0]))
-	.attr("y1", yscale(circle.point[1]))
-	.attr("x2", xscale(1))
-	.attr("y2", yscale(circle.point[1]))
-	.attr("stroke", "#333");
 
-svg.append("text")
-	.attr("class", "radius-text")
-	.attr("x", xscale(0.75))
-	.attr("y", yscale(0.49))
-	.text("r");
+
+
+// svg.append("line")
+// 	.attr("class", "radius-line")
+// 	.attr("x1", xscale(circle.point[0]))
+// 	.attr("y1", yscale(circle.point[1]))
+// 	.attr("x2", xscale(1))
+// 	.attr("y2", yscale(circle.point[1]))
+// 	.attr("stroke", "#333");
+
+// svg.append("text")
+// 	.attr("class", "radius-text")
+// 	.attr("x", xscale(0.75))
+// 	.attr("y", yscale(0.49))
+// 	.text("r");
 	
-svg.selectAll("*").style("opacity", 0);
+svg.selectAll("*").style("opacity", 1);
